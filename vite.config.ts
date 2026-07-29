27fbc2dfbb7bd217d0c2964e1af26b8ddb2118bb
AriefWPS <arief.bogaro@gmail.com>
2026-07-30 01:17:24 +0700

    perbaiki base config untuk github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // Pastikan import ini benar jika pakai Tailwind v4
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Web-Porto-Arief/', // <--- Kembalikan menggunakan nama repository
+  // BARIS INI SANGAT PENTING UNTUK GITHUB PAGES:
+  base: '/Web-Porto-Arief/', // <--- Gunakan tanda garis miring di awal dan akhir
 })
